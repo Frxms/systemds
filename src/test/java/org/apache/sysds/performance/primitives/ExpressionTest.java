973a486ba79e25082f6e698891db05ae2b56918e
Frxms <tomoki.mengel@gmail.com>
2025-08-14 20:03:50 +0200

    create new test class

---
+++ src/test/java/org/apache/sysds/performance/primitives/ExpressionTest.java
@@ -0,0 +1,127 @@
+package org.apache.sysds.performance.primitives;
+
+import org.apache.sysds.api.DMLScript;
+import org.apache.sysds.api.jmlc.Connection;
+import org.apache.sysds.api.jmlc.PreparedScript;
+import org.apache.sysds.common.Types;
+import org.apache.sysds.conf.CompilerConfig;
+import org.apache.sysds.conf.DMLConfig;
+import org.apache.sysds.hops.OptimizerUtils;
+import org.apache.sysds.performance.TimingUtils;
+import org.apache.sysds.test.TestUtils;
+import org.apache.sysds.utils.Explain;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ExpressionTest {
+
+	private static final String TEST_NAME = "expression";
+	private static final String TEST_NAME1 = TEST_NAME+"1";
+	private static final String TEST_NAME2 = TEST_NAME+"2";
+
+	private static final String TEST_DIR = "./src/test/scripts/performance/primitives/";
+
+	private final static double sparsity1 = 0.9;
+	private final static double sparsity2 = 0.1;
+	private final static double eps = 1e-8;
+	private int rows = 2000;
+	private int cols = 10000;
+	double[] sparsities = new double[] {1, 0.3333333, 0.1111111, 0.0333333, 0.0111111, 0.0033333, 0.0011111, 0.0003333, 0.0001111, 0.0000333, 0.0000111, 0.0000033, 0.0000011, 0.0000003, 0.0000001};
+	int warmupRuns = 100;
+	int repetitions = 2500;
+
+	public static void main(String[] args) {
+//		new ExpressionTest().runSparseBenchmark(TEST_NAME1);
+		new ExpressionTest().runDenseBenchmark(TEST_NAME1);
+	}
+
+	public void runSparseBenchmark(String testname) {
+		logResults(runPerfTest(testname,  true, Types.ExecType.CP), true);
+	}
+
+	public void runDenseBenchmark(String testname) {
+		logResults(runPerfTest(testname, false, Types.ExecType.CP), false);
+	}
+
+	public void runBenchmark(String testname, int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+		logResults(runPerfTest(testname,  true, Types.ExecType.CP), true);
+		logResults(runPerfTest(testname, false, Types.ExecType.CP), false);
+	}
+
+	private String[] runPerfTest(String testname, boolean sparseRowVec, Types.ExecType et) {
+
+		String[] resultTime = new String[sparsities.length];
+
+		Connection conn = new Connection(new DMLConfig(), CompilerConfig.ConfigType.CODEGEN_ENABLED);
+		DMLScript.EXPLAIN = Explain.ExplainType.CODEGEN;
+		boolean oldSparse = DMLScript.SPARSE_INTERMEDIATE;
+		DMLScript.SPARSE_INTERMEDIATE = sparseRowVec;
+
+		try {
+
+			String HOME = TEST_DIR;
+			String script = DMLScript.readDMLScript(true, HOME + testname + ".dml");
+
+			String[] inputs = new String[] {"A", "v", "B"};
+			PreparedScript pscript = conn.prepareScript(script, inputs, new String[] {"S"});
+
+			for(int i = 0; i < sparsities.length; i++) {
+				double[][] A = TestUtils.generateTestMatrix(rows, cols, 1, 31, sparsities[i], 1234);
+				double[][] B = TestUtils.generateTestMatrix(rows, cols, 1, 31, sparsities[i], 5678);
+				double[][] V = TestUtils.generateTestMatrix(rows, 1, 1, 31, sparsities[i], 9876);
+
+				pscript.setMatrix("A", A, true);
+				pscript.setMatrix("B", B, true);
+				pscript.setMatrix("v", V, true);
+
+				TimingUtils.time(() -> pscript.executeScript(), warmupRuns);
+				double[] result = TimingUtils.time(() -> pscript.executeScript(), repetitions);
+
+				resultTime[i] = TimingUtils.stats(result).split("\\+-")[0];
+
+				System.out.println(TimingUtils.stats(result).split("\\+-")[0]);
+
+				pscript.clearPinnedData();
+			}
+
+			DMLScript.SPARSE_INTERMEDIATE = oldSparse;
+			conn.close();
+		}
+		catch(IOException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			OptimizerUtils.ALLOW_AUTO_VECTORIZATION = true;
+			OptimizerUtils.ALLOW_OPERATOR_FUSION = true;
+		}
+		return resultTime;
+	}
+
+	public void logResults(String[] result, boolean sparseInterm) {
+		String currDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyy_HHmmss"));
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(new FileWriter("C:\\Users\\tomok\\OneDrive - Technische Universität Berlin\\Bachelorarbeit\\performance\\expression_testing\\"
+				+ "expressionTest_"+ currDate + "_" + (sparseInterm ? "sparseInterm" : "denseInterm") + "_" + ".csv"));
+		}
+		catch(IOException e) {
+			throw new RuntimeException(e);
+		}
+		writer.printf("Repetitions: %1$2s, rl: %2$2s, cl: %3$2s%n", repetitions, rows, cols);
+		writer.printf("%1$2s;%2$2s%n", "Sparsity", "time in ms");
+		for(int i = 0; i < sparsities.length; i++) {
+			writer.printf("%1$2s;%2$2s%n",  sparsities[i], result[i]);
+		}
+
+		writer.flush();
+		writer.close();
+	}
+
+}
